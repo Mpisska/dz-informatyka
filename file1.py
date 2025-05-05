fa20232bfcbb87c4783e6e2a7b5b7000ae015160
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2025-05-05 13:38:55 +0200

    add zadanie 4,5,6

--- file1.py
+++ file1.py
@@ -22,3 +22,27 @@ kurs = 4.74
 cena = 126
 zloty = kurs * cena
 print(f'Do zapłaty: {zloty} zł')
+
+#4
+kurs =  4.76
+cenaP = 200
+cenaG = 49 * kurs
+if cenaG < cenaP:
+    print('W Grecji taniej')
+else:
+    print('w Polsce taniej')
+
+#5
+day1 = 12
+day2 = 14
+day3 = 18
+suma = day1+day2+day3
+srednia = suma/2
+
+#6
+def jak_szybko(t, s):
+    srednia_predkosc = s / t
+    return srednia_predkosc
+
+print(f"Wynik 1 {jak_szybko(2.5, 1320)}")
+print(f"Wynik 2: {jak_szybko(3, 1320)}")
