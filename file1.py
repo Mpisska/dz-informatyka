8b52f14b2531142f4e9346dc49141477e5e06a22
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2025-05-05 12:31:59 +0200

    add zadanie1,2

--- file1.py
+++ file1.py
@@ -1,5 +1,18 @@
+#1
 haslo1 = "Podróże kształcą i otwierają umysł."
 haslo2 = "Świat czeka na odkrycie – ruszaj w drogę!"
 haslo3 = "Każda podróż to nowa historia do opowiedzenia."
 
 print(haslo1 + "\n" + haslo2 + "\n" + haslo3)
+
+#2
+a = 10
+b = 3
+
+suma = a + b
+roznica = a - b
+iloczyn = a * b
+iloraz = a / b
+iloraz_calkowity = a // b
+reszta = a % b
+kwadrat = a ** 2
