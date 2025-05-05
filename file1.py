b1b7cd1026dacf895a7701e545cbf946f9dfd9d1
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2025-05-05 12:29:53 +0200

    modified file1

---
+++ file1.py
@@ -0,0 +1,5 @@
+haslo1 = "Podróże kształcą i otwierają umysł."
+haslo2 = "Świat czeka na odkrycie – ruszaj w drogę!"
+haslo3 = "Każda podróż to nowa historia do opowiedzenia."
+
+print(haslo1 + "\n" + haslo2 + "\n" + haslo3)
