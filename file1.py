a3a30567f2b670036e216f340635d04e12494592
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2025-05-07 12:06:19 +0200

    zadanie 7-24

--- file1.py
+++ file1.py
@@ -46,3 +46,89 @@ def jak_szybko(t, s):
 
 print(f"Wynik 1 {jak_szybko(2.5, 1320)}")
 print(f"Wynik 2: {jak_szybko(3, 1320)}")
+
+#7
+def zamiana(m):
+    km = m * 1.609
+    return km
+
+print(f'Wynik: {zamiana(10)}')
+print(f'Wynik: {zamiana(12)}')
+
+#8
+def godzina(n):
+    wyjazd = 21
+    przyjazd = (wyjazd + n)%24
+    return przyjazd
+print(f'wynik: {godzina(14)}')
+print(f'wynik: {godzina(20)}')
+
+#9
+def godz_nj(g):
+    return (g - 6) % 24
+print(f'godz_nj(18) = {godz_nj(18)}')
+print(f'godz_nj(2) =  {godz_nj(2)}')
+
+#10
+def pole_kw(bok):
+    return bok * bok
+print(pole_kw(25))
+print(pole_kw(101))
+
+#14
+def cena(f):
+    usd = 4.75
+    funt = 0.45
+    return (f*usd)/funt
+print(cena(1.35))
+
+#15
+x = 3
+y = 5
+x, y = y, x
+
+#16
+n = int(input('Enter n: '))
+if n%3 == 0 and n%7 ==0:
+    print('yes')
+else:
+    print('no')
+
+#20
+def lata(ile):
+    if ile < 18:
+        print(0)
+    elif ile > 50:
+        print(2)
+    else:
+        print(1)
+print(lata(14))
+
+#21
+def bmi(kg, m):
+    bmi = kg/(m * m)
+    if bmi < 18.5:
+        print('niedowaga')
+    elif bmi > 25:
+        print('nadwaga')
+    else:
+        print('prawidlowa waga')
+print(bmi(50, 1.63) )
+
+#22
+def naj(liczba1, liczba2, liczba3):
+    return max(liczba1, liczba2, liczba3)
+print(naj(1, 2, 3))
+
+#23
+def waga(pocz, n):
+    przyrost = n / 2 * (2 * 2 + (n - 1) * 2)
+    masa_koncowa = pocz + przyrost
+    return masa_koncowa
+print(waga(2, 2))
+
+#24
+def skarbonka(ile, n):
+    suma_wrzut = 2 ** n - 1
+    return ile + suma_wrzut
+print(skarbonka(12, 2))
