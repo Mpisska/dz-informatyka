962ac2f6985541495c24a099fe390514c99c21e8
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2025-05-12 22:40:09 +0200

    clear main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-print('love')
-name = input('enter your name: ').capitalize()
-if name == 'Настя':
-    print('Шлюха')
-elif name == 'Никита':
-    print('Еблан')
-else:
-    print('Поздравляю у тебя норм имя')
